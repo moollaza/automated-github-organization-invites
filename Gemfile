b2858d4e32c1c7a1a7d2648f3f6839786f8969d5
Zaahir Moolla <moollaza@duckduckgo.com>
2017-03-21 14:51:32 -0400

    better logic for handling api response

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem 'dotenv'
+gem "dotenv"
 gem "sinatra"
 gem "octokit"
 gem "slim"
--- web_app.rb
+++ web_app.rb
@@ -6,53 +6,90 @@ require 'slim'
 require 'octokit'
 
 token = ENV['GITHUB_TOKEN']
+if token == nil
+    abort("Please provide a GitHub Token!")
+end
+
 team_id = ENV['TEAM_ID']
+if team_id == nil
+    abort("Please provide a GitHub Team ID!")
+end
+
 
 client = Octokit::Client.new(access_token: token)
 
+# Check user is real
 def user_exists?(client, user)
-  begin
-    response = client.user(user)
-  rescue Octokit::NotFound
-    return false
-  end
-  return true
+    begin
+        response = client.user(user)
+        return true
+    rescue Octokit::NotFound
+        return false
+    end
 end
 
+# Check if user is already a team member
 def is_team_member?(client, team_id, user)
-  begin
-    response = client.team_membership(team_id, user)
-    return response.state
-  rescue Octokit::NotFound
-    return false
-  end
+    begin
+        response = client.team_membership(team_id, user)
+        return response.state
+    rescue Octokit::NotFound
+        return false
+    end
+end
+
+# Add user to team
+def add_team_member?(client, team_id, user)
+    begin
+        response = client.add_team_membership(client, team_id, user)
+        return response
+    rescue
+        puts 'add_team_member FAILED'
+        return false
+    end
 end
 
 
 # ROUTES #
 
-get "/" do
-  slim :index, :locals => { :profile => @profile }
+get '/' do
+    slim :index, locals: { profile: @profile }
 end
 
-post "/add" do
-  username = params["username"]
-  if user_exists?(client, username)
-    membership = is_team_member?(client, team_id, username)
-    if membership == "active"
-      heading = "#{username} is already an active member of the team!"
-      message = nil
-    elsif membership == "pending"
-      heading = "#{username}'s team membership is pending"
-      message = "Please check your email for an invitation.<br>Follow the instructions to finish joining the team."
+
+post '/add' do
+    @username = params['username']
+    # Check if user is real
+    if user_exists?(client, @username)
+        membership = is_team_member?(client, team_id, @username)
+
+        # Already team member
+        if membership == 'active'
+            heading = "#{@username} is already an active member of the team!"
+            message = nil
+
+        # Invite already sent
+        elsif membership == 'pending'
+            heading = "#{@username}'s team membership is pending"
+            message = 'Please check your email for an invitation.<br>Follow the instructions to finish joining the team.'
+
+        # Not a team member
+        else
+            response = add_team_member?(client, team_id, @username)
+
+            # Invitation failed
+            if response == false
+                heading = "Uh oh! We were unable to add #{@username} to the team!"
+                message = 'Please contact open@duckduckgo.com to let us know.'
+            # Invitation successfully sent
+            elsif response.state == 'pending'
+                heading = "Sweet! You're almost done..."
+                message = 'Please check your email for an invitation.<br>Follow the instructions to finish joining the team.'
+            end
+        end
     else
-      client.add_team_membership(client, team_id, username)
-      heading = "Sweet! You're almost done..."
-      message = 'Please check your email for an invitation.<br>Follow the instructions to finish joining the team.'
+        heading = 'Uh oh!'
+        message = "User: '#{@username}' not found. Please check your spelling."
     end
-  else
-    heading = "Uh oh!"
-    message = "User not found. Please check your spelling"
-  end
-  slim :result, :locals => { :heading => heading, :message => message }
+    slim :result, locals: { heading: heading, message: message }
 end
